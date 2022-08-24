7da7690d79e64f00e9d3536031aef39efd94f757
Alternmill <vitalik0969870505@gmail.com>
2022-08-24 17:58:33 +0300

    Added glass material

--- material.h
+++ material.h
@@ -50,4 +50,36 @@ public:
 
 };
 
+//TODO:
+//Different colors
+class dielectric : public material{
+public:
+    double ir;
+
+    dielectric(double index_of_refraction) : ir(index_of_refraction){}
+
+    virtual bool scatter(
+        const ray& r_in,const hit_record& rec,color& attenuatuion, ray& scattered
+    )const override{
+        attenuatuion = color(0.9,0.9,0.9);
+        double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+
+        double cos_theta = fmin(dot(-unit_direction,rec.normal),1.0);
+        double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+
+        vec3 direction;
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        
+        if(cannot_refract)
+            direction = reflect(unit_direction,rec.normal);
+        else 
+            direction = refract(unit_direction,rec.normal,refraction_ratio);
+
+        scattered = ray(rec.p,direction);
+        return true;
+    }
+};
+
 #endif
--- vec3.h
+++ vec3.h
@@ -130,6 +130,13 @@ vec3 random_unit_vector(){
     return unit_vector(random_in_sphere());
 }
 
+vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat){
+    auto cos_theta = fmin(dot(-uv,n),1.0);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta*n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_sq())) * n;
+    return r_out_perp + r_out_parallel;
+}
+
 using color = vec3;
 using point3 = vec3;
 
